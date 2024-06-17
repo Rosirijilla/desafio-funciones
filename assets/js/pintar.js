3cfc7bebebbb781295cbb707677f74c6d532362c
Rosa Jelves <rosa.sakura.j@gmail.com>
2024-06-16 22:10:20 -0400

    corrección de algunas cosas pequeñas

--- assets/js/pintar.js
+++ assets/js/pintar.js
@@ -9,20 +9,14 @@ ele.addEventListener("click", pintar);
 
  */
  
- 
- 
- // Función para cambiar el color de un elemento
  function pintar(elemento, color = 'green') {
     elemento.style.backgroundColor = color;
 }
 
-// Seleccionamos el elemento
 const ele = document.getElementById("ele1");
 
-// Aplicamos el color verde por defecto al cargar la página
 pintar(ele);
 
-// Añadimos un evento de clic para cambiar el color a amarillo
 ele.addEventListener("click", function() {
     pintar(ele, 'yellow');
 });
